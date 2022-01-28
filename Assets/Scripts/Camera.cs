61e9d7d55925794626b5b7e6153f08f362d2ab76
Skyman402 <98478824+Skyman402@users.noreply.github.com>
2022-01-28 13:23:49 +0300

    123

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    private void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
